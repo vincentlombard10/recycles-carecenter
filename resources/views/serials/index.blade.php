3d53eedb777adbbc2a218ac635705bd2ecc9baf0
vincentlombard10 <vincent.lombard@re-cycles-france.fr>
2025-08-21 11:53:13 +0200

    commit

--- resources/views/serials/index.blade.php
+++ resources/views/serials/index.blade.php
@@ -4,6 +4,8 @@
     </x-page-header>
     <x-page-wrapper>
         <livewire:search-form />
+        @if(\App\Models\Serial::count())
         <livewire:serials-index />
+        @endif
     </x-page-wrapper>
 </x-app-layout>
