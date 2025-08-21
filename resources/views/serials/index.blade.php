13716072cdf991848d26951f0afea00c71f31945
vincentlombard10 <vincent.lombard@re-cycles-france.fr>
2025-08-21 11:55:49 +0200

    commit

--- resources/views/serials/index.blade.php
+++ resources/views/serials/index.blade.php
@@ -3,11 +3,11 @@
         <h1>Numéros de série</h1>
     </x-page-header>
     <x-page-wrapper>
-        <livewire:search-form />
         @if(\App\Models\Serial::count() > 0)
-        <livewire:serials-index />
+            <livewire:search-form />
+            <livewire:serials-index />
         @else
-            No serial
+            <div class="alert">Il ne se passe pas grand chose ici.</div>
         @endif
     </x-page-wrapper>
 </x-app-layout>
