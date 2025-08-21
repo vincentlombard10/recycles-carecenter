437f4c9273b27193f7fc128ba4ecafb7884b12be
vincentlombard10 <vincent.lombard@re-cycles-france.fr>
2025-08-21 15:38:15 +0200

    commit

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -59,6 +59,11 @@
                class="sidebar-link {{ Route::is('contacts.*') ? 'active': '' }}"><i class="bi bi-person-lines-fill"></i>&nbsp;<span
                     class="sidebar-link__label">Contacts</span></a>
         </li>
+        <li class="sidebar-item">
+            <a href="{{ route('contacts.index') }}"
+               class="sidebar-link {{ Route::is('log-viewer.*') ? 'active': '' }}"><i class="bi bi-person-lines-fill"></i>&nbsp;<span
+                    class="sidebar-link__label">Visualiseur de logs</span></a>
+        </li>
     </ul>
     @endhasanyrole
     <ul class="sidebar-menu mt-auto mb-0">
