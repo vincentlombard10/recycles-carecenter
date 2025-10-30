ca04b3ce04dc5a729c40550808d574ed5024c37e
vincentlombard10 <vincent.lombard@re-cycles-france.fr>
2025-10-30 13:41:54 +0100

    commit

--- app/Imports/SerialsImport.php
+++ app/Imports/SerialsImport.php
@@ -55,7 +55,7 @@ class SerialsImport implements ToCollection, WithHeadingRow, WithBatchInserts, W
                 'last_order' => $row['snh_cdem3'],
                 'last_delivery' => $row['snh_bl'],
                 'dealer_code' => $row['snh_cli'],
-                'item_id' => Item::where('code', $row['snh_art'])->first()->id ?? null,
+                'item_id' => Item::where('itno', $row['snh_art'])->first()->id ?? null,
             ]);
         }
     }
