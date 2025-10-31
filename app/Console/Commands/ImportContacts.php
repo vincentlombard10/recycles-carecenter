69621ad12593b33f3bf42f5b4b1a8637fbf66f6c
vincentlombard10 <vincent.lombard@re-cycles-france.fr>
2025-10-31 11:06:40 +0100

    commit

--- app/Console/Commands/ImportContacts.php
+++ app/Console/Commands/ImportContacts.php
@@ -56,7 +56,7 @@ class ImportContacts extends Command
 
         try {
 
-            $fileContents = Storage::disk('sftp')->get($filename);
+            $fileContents = Storage::disk('m3files-ftp')->get($filename);
             Log::debug('FileContents', ['contents' =>$fileContents]);
 
             if (!$fileContents) {
