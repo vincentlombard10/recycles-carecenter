11281aa0722050ff3d0a45386c0cf7fdd0610d97
vincentlombard10 <vincent.lombard@re-cycles-france.fr>
2025-11-04 10:57:42 +0100

    commit

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -6,27 +6,27 @@
         <input type="hidden" name="token" value="{{ $request->route('token') }}">
 
         <!-- Email Address -->
-        <div>
+        <div class="mb-3">
             <label for="email" class="form-label mb-1">Adresse e-mail</label>
             <input type="text" class="form-control" name="email" value="{{ old('email', $request->email) }}" required autofocus>
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
         <!-- Password -->
-        <div class="mt-4">
+        <div class="mb-3">
             <label for="password" class="form-label mb-1">Nouveau mot de passe</label>
             <input type="password" name="password" class="form-control" required>
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
         <!-- Confirm Password -->
-        <div class="mt-4">
+        <div class="mb-3">
             <label for="password_confirmation" class="form-label">Confirmation du mot de passe :</label>
             <input type="password" name="password_confirmation" class="form-control" required>
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
-        <div class="flex items-center justify-end mt-4">
+        <div class="d-grid">
             <input type="submit" value="Réinitiailiser le mot de passe" class="btn btn-primary">
         </div>
     </form>
