54453406e4a9fd682a45b7c58096c82366cae86f
vincentlombard10 <vincent.lombard@re-cycles-france.fr>
2025-11-04 11:41:36 +0100

    commit

--- resources/lang/fr/passwords.php
+++ resources/lang/fr/passwords.php
@@ -3,4 +3,5 @@
 return [
     'sent' => 'Lien de renouvellement de mot de passe envoyé.',
     'user' => 'Aucun utilisateur avec cette adresse e-mail.',
+    'throttled' => 'Veuillez attendre quelques instants avant de redemander un nouveau mot de passe.',
 ];
