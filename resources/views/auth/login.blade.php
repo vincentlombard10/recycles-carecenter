ac972e3c1d0d6ecda164d3d42a694ae22a3db374
vincentlombard10 <vincent.lombard@re-cycles-france.fr>
2025-11-04 11:04:06 +0100

    commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,10 +10,10 @@
         {{ html()->password('password')->class('form-control focus-ring focus-ring-light')->autocomplete('current-password') }}
         <x-input-error :messages="$errors->get('password')" class="mt-2"/>
     </div>
-    <div class="mb-3">
+    <div class="d-grid mb-3">
         {{ html()->submit('Connexion')->class('btn btn-primary ') }}
     </div>
-    <div class="d-grid">
+    <div>
         @if (Route::has('password.request'))
             <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
                href="{{ route('password.request') }}">
