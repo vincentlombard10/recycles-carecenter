dd41b3f1a93e98425893fe3551450a7e4dc75865
vincentlombard10 <vincent.lombard@re-cycles-france.fr>
2025-11-04 10:47:27 +0100

    commit

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,6 +1,7 @@
 <x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+    <div class="mt-4 mb-4 text-sm text-gray-600 dark:text-gray-400">
+        <h6>Vous avez oublié vote mot de passe ?</h6>
+        <p>Aucun souci, renseignez votre adresse e-mail et vous recevrez un lien pour renouveler votre mot de passe.</p>
     </div>
 
     <!-- Session Status -->
@@ -8,18 +9,14 @@
 
     <form method="POST" action="{{ route('password.email') }}">
         @csrf
-
-        <!-- Email Address -->
         <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+            <label for="email" class="form-label mb-1">Adresse e-mail</label>
+            <input type="text" name="email" class="form-control" value="{{ old('email') }}" required autofocus>
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
-            </x-primary-button>
+        <div class="d-grid items-center justify-end mt-2">
+            <input type="submit" class="btn btn-primary" value="Recevoir le lien de renouvellement.">
         </div>
     </form>
 </x-guest-layout>
--- routes/zendesk.php
+++ routes/zendesk.php
@@ -6,7 +6,7 @@ use App\Models\Zendesk\TicketField;
 use Zendesk\API\HttpClient as ZendeskAPI;
 
 Route::group(['prefix' => 'zendesk', 'as' => 'zendesk.'], function () {
-    Route::group(['prefix' => '/tickets', 'as' => 'tickets.'], function() {
+    Route::group(['prefix' => '/tickets', 'as' => 'tickets.'], function () {
         Route::get('/import', \App\Http\Controllers\Zendesk\ImportZendeskTicketController::class)
             ->name('import');
         Route::post('/export', \App\Http\Controllers\Zendesk\ExportZendeskTicketController::class)
