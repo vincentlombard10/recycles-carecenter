5959ff84f5ba14620832aced31edc4d66b74e6a4
vincentlombard10 <vincent.lombard@re-cycles-france.fr>
2025-11-12 15:43:32 +0100

    commit

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Role;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
 
 class UserController extends Controller
