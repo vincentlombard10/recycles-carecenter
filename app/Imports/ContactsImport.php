221cfea1e5a27113ae778abf2802c487d03f4aab
vincentlombard10 <vincent.lombard@re-cycles-france.fr>
2025-11-12 16:16:24 +0100

    commit

--- app/Imports/ContactsImport.php
+++ app/Imports/ContactsImport.php
@@ -3,39 +3,49 @@
 namespace App\Imports;
 
 use App\Models\Contact;
-use Maatwebsite\Excel\Concerns\ToModel;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Log;
+use Maatwebsite\Excel\Concerns\ToCollection;
 use Maatwebsite\Excel\Concerns\WithBatchInserts;
 use Maatwebsite\Excel\Concerns\WithChunkReading;
 use Maatwebsite\Excel\Concerns\WithEvents;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
 use Maatwebsite\Excel\Concerns\WithUpserts;
+use Matrix\Exception;
 
-class ContactsImport implements ToModel, WithHeadingRow, WithBatchInserts, WithChunkReading, WithUpserts, WithEvents
+class ContactsImport implements ToCollection, WithHeadingRow, WithBatchInserts, WithChunkReading, WithUpserts, WithEvents
 {
     /**
     * @param array $row
     *
     * @return \Illuminate\Database\Eloquent\Model|null
     */
-    public function model(array $row)
+
+    public function collection(Collection $collection)
     {
-        if ($row['cli_code'] !== 'Cli_Code' && intval($row['cli_sta']) === 20) {
-            return new Contact([
-                'code' => $row['cli_code'],
-                'name' => $row['cli_nom'],
-                'email' => $row['cli_mail'],
-                'status' => intval($row['cli_sta']) ?? null,
-                'address1' => $row['cli_adr1'],
-                'address2' => $row['cli_adr2'],
-                'postcode' => $row['cli_codepos'],
-                'city' => $row['cli_ville'],
-                'country' => $row['cli_pays'],
-                'phone' => $row['cli_tel'],
-                'salesrep' => $row['cli_rep'],
-            ]);
+        foreach ($collection as $row) {
+            if ($row['cli_code'] !== 'Cli_Code' && intval($row['cli_sta']) === 20) {
+                try {
+                    $contact = Contact::updateOrCreate([
+                        'code' => $row['cli_code'],
+                    ], [
+                        'name' => $row['cli_nom'],
+                        'email' => $row['cli_mail'],
+                        'status' => intval($row['cli_sta']) ?? null,
+                        'address1' => $row['cli_adr1'],
+                        'address2' => $row['cli_adr2'],
+                        'postcode' => $row['cli_codepos'],
+                        'city' => $row['cli_ville'],
+                        'country' => $row['cli_pays'],
+                        'phone' => $row['cli_tel'],
+                        'salesrep' => $row['cli_rep'],
+                    ]);
+                } catch (Exception $e) {
+                    Log::error($e->getMessage());
+                }
+            }
         }
     }
-
     public function batchSize(): int
     {
         return 1000;
