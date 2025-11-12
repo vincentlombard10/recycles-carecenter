2d359579c417a2db1c29ea6b002ce0124865ced8
vincentlombard10 <vincent.lombard@re-cycles-france.fr>
2025-11-12 09:17:53 +0100

    commit

--- resources/views/livewire/product-reports/index.blade.php
+++ resources/views/livewire/product-reports/index.blade.php
@@ -41,7 +41,8 @@
                 <div class="Card_Support--Side">
                     <div class="d-grid gap-1">
                         <span class="rcf-badge rcf-badge--{{ $report->status_color }}">{{ $report->status_label }}</span>
-                        @if($report->status !== 'init')
+                        @if($report->status !== 'init'
+                            && auth()->user()->canAny('reports.update', 'reports.download'))
                             <div class="btn-group">
                                 <button type="button" class="btn btn-light dropdown-toggle" data-bs-toggle="dropdown"
                                         aria-expanded="false">
