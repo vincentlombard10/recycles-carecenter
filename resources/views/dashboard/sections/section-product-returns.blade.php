d4225e6d76714306c4e62ec64feba4586890b253
vincentlombard10 <vincent.lombard@re-cycles-france.fr>
2025-11-13 13:39:46 +0100

    commit

--- resources/views/dashboard/sections/section-product-returns.blade.php
+++ resources/views/dashboard/sections/section-product-returns.blade.php
@@ -1,7 +1,11 @@
 <section>
     <div class="row">
         <div class="col-12 mb-3">
-            <h2>Retours produits</h2>
+            <div class="card">
+                <div class="card-body">
+                    <h2>Retours produits</h2>
+                </div>
+            </div>
         </div>
     </div>
 </section>
