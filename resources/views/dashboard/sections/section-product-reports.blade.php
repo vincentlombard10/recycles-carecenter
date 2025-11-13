31f1b96a6d9220086a3d215cb84626fbc692e271
vincentlombard10 <vincent.lombard@re-cycles-france.fr>
2025-11-13 14:22:27 +0100

    commit

--- resources/views/dashboard/sections/section-product-reports.blade.php
+++ resources/views/dashboard/sections/section-product-reports.blade.php
@@ -1,6 +1,6 @@
 <section class="row">
     <h2>Expertises et rapports</h2>
-    <div class="col-4 mb-3">
+    <div class="col-2 mb-3">
         <x-dashboard.counter
             title="en cours"
             :count="$product_reports_in_progress_count" />
@@ -10,7 +10,7 @@
             title="terminés"
             :count="$product_reports_closed_count" />
     </div>
-    <div class="col-4 mb-3">
+    <div class="col-2 mb-3">
         <x-dashboard.counter
             title="temps moyen de traitement"
             :count="$product_reports_duration_time" />
