25dc0af18c8527275b60bc57aa740648e522daed
vincentlombard10 <vincent.lombard@re-cycles-france.fr>
2025-11-13 14:26:14 +0100

    commit

--- resources/views/dashboard/sections/section-items.blade.php
+++ resources/views/dashboard/sections/section-items.blade.php
@@ -1,6 +1,6 @@
 <section class="row">
     <h2>Références produits</h2>
-    <div class="col-4 mb-3">
+    <div class="col-3 mb-3">
         <x-dashboard.counter
             title="tous"
             :count="$items_count" />
--- resources/views/dashboard/sections/section-serials.blade.php
+++ resources/views/dashboard/sections/section-serials.blade.php
@@ -1,6 +1,6 @@
 <section class="row">
     <h2>Numéros de série</h2>
-    <div class="col-4 mb-3">
+    <div class="col-3 mb-3">
         <x-dashboard.counter
             title="tous"
             :count="$serials_count" />
