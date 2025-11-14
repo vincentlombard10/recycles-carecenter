d894bf5f05d61101fd2c2c62608beea9b640849c
vincentlombard10 <vincent.lombard@re-cycles-france.fr>
2025-11-14 16:05:50 +0100

    commit

--- resources/js/components/ProductReport/sections/ReportStatusSection.vue
+++ resources/js/components/ProductReport/sections/ReportStatusSection.vue
@@ -3,14 +3,14 @@
         <VAlert v-if="store.reportCompleted" type="success">Le rapport d'intervention est complet.</VAlert>
         <VAlert v-else>Complétez le rapport pour terminer l'intervention.</VAlert>
     </section>
-    <section class="Section" v-if="store.reportCompleted">
+    <section class="Section">
         <div class="row">
             <div class="col-6">
                 <label for="status" class="form-label mb-1">Statut</label>
                 <select name="status" id="status" class="form-control">
                     <option value="in_progress">En cours</option>
                     <option value="paused">En pause - devis en attente d'approbation</option>
-                    <option value="closed">Terminé</option>
+                    <option value="closed" v-if="store.reportCompleted">Terminé</option>
                 </select>
             </div>
         </div>
