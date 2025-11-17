2e347acb5b6e1467103fc4c148dda5f4590beea9
vincentlombard10 <vincent.lombard@re-cycles-france.fr>
2025-11-17 14:12:53 +0100

    commit

--- resources/views/components/card-component.blade.php
+++ resources/views/components/card-component.blade.php
@@ -1,19 +1,15 @@
 <div class="Card_Item p-0 mb-2">
     <div class="title mb-1">Composant concerné</div>
-    @if($item->item)
-        <table class="table table-mini">
-            <tbody>
-            <tr>
-                <th style="width: 6rem;">Référence</th>
-                <td><span class="fw-semibold">{{ $item->item_itno }}</span></td>
-            </tr>
-            <tr>
-                <th>Désignation</th>
-                <td><span class="">{{ $item->item_itds }}</span></td>
-            </tr>
-            </tbody>
-        </table>
-    @else
-        <div>Référence : {{ $item->item_itno }}</div>
-    @endif
+    <table class="table table-mini">
+        <tbody>
+        <tr>
+            <th style="width: 6rem;">Référence</th>
+            <td><span class="fw-semibold">{{ $item->item_itno }}</span></td>
+        </tr>
+        <tr>
+            <th>Désignation</th>
+            <td><span class="">{{ $item->item_itds }}</span></td>
+        </tr>
+        </tbody>
+    </table>
 </div>
