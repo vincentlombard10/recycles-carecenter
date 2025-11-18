7e23a46fbb3e3b0151f02de4a6188e7809592f18
vincentlombard10 <vincent.lombard@re-cycles-france.fr>
2025-11-18 16:16:15 +0100

    commit

--- resources/views/dashboard/sections/section-product-returns.blade.php
+++ resources/views/dashboard/sections/section-product-returns.blade.php
@@ -3,21 +3,25 @@
     <div class="col-3 mb-3">
         <x-dashboard.counter
             title="tous"
+            class="Violet"
             :count="$product_returns_count" />
     </div>
     <div class="col-3 mb-3">
         <x-dashboard.counter
             title="en attente"
+            class="Pending"
             :count="$product_returns_pending_count" />
     </div>
     <div class="col-3 mb-3">
         <x-dashboard.counter
             title="reçus"
+            class="Received"
             :count="$product_returns_received_count" />
     </div>
     <div class="col-3 mb-3">
         <x-dashboard.counter
             title="Sandbox"
+            class="Sandbox"
             :count="$product_returns_sandboxed_count" />
     </div>
 </section>
