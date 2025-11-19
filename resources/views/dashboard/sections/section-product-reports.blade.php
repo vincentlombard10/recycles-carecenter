32961f28d1bf65b2b0ac17ed267d52ca92b6eb5d
vincentlombard10 <vincent.lombard@re-cycles-france.fr>
2025-11-19 10:20:47 +0100

    commit

--- resources/views/dashboard/sections/section-product-reports.blade.php
+++ resources/views/dashboard/sections/section-product-reports.blade.php
@@ -14,6 +14,6 @@
         <x-dashboard.counter
             title="traitement moyen"
             suffix="min"
-            :count="$product_reports_duration_time > 0 ? round($product_reports_duration_time/60) : null" />
+            :count="$product_reports_duration_time > 0 ? $product_reports_duration_time/60 : null" />
     </div>
 </section>
