849c17bf04ef7f7ebc1eada225473e3531861cfa
vincentlombard10 <vincent.lombard@re-cycles-france.fr>
2025-11-19 10:49:13 +0100

    commit

--- resources/views/pdf/product-returns/partials/routing-from.blade.php
+++ resources/views/pdf/product-returns/partials/routing-from.blade.php
@@ -1,4 +1,4 @@
 <div>{{ strtoupper($productReturn->routing_from_address1) }}</div>
 <div>{{ strtoupper($productReturn->routing_from_address2) }}</div>
 <div>{{ $productReturn->routing_from_postcode }} {{ strtoupper($productReturn->routing_from_city) }}</div>
-<div>{{ $productReturn->routing_from_phone }}</div>
+<div>Tél : {{ $productReturn->routing_from_phone }}</div>
--- resources/views/pdf/product-returns/partials/routing-to.blade.php
+++ resources/views/pdf/product-returns/partials/routing-to.blade.php
@@ -1,4 +1,4 @@
 <div>{{ strtoupper($productReturn->routing_to_address1) }}</div>
 <div>{{ strtoupper($productReturn->routing_to_address2) }}</div>
 <div>{{ $productReturn->routing_to_postcode }} {{ strtoupper($productReturn->routing_to_city) }}</div>
-<div>{{ $productReturn->routing_to_phone }}</div>
+<div>Tél : {{ $productReturn->routing_to_phone }}</div>
