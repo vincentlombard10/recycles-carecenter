b5eaf99802b9ca4f3f5199cd80eac69f17f05b27
vincentlombard10 <vincent.lombard@re-cycles-france.fr>
2025-11-19 09:47:27 +0100

    modification du fichier console.php

--- routes/console.php
+++ routes/console.php
@@ -3,11 +3,11 @@
 use App\Models\Ticket;
 use Zendesk\API\HttpClient as ZendeskAPI;
 
-Schedule::command('items:import oulala ' . \Carbon\Carbon::now()->format('ymd'))
+Schedule::command('items:import ' . \Carbon\Carbon::now()->format('ymd'))
     ->dailyAt('20:00');
 
-Schedule::command('serials:import --file=' . \Carbon\Carbon::now()->format('ymd'))
+Schedule::command('serials:import ' . \Carbon\Carbon::now()->format('ymd'))
     ->dailyAt('20:05');
 
-Schedule::command('contacts:import --file=' . \Carbon\Carbon::now()->format('ymd'))
+Schedule::command('contacts:import ' . \Carbon\Carbon::now()->format('ymd'))
     ->dailyAt('20:10');
