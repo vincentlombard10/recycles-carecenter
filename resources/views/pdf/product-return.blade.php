c096eb9bd5a36f577ae344b40d938d0d2cbb573e
vincentlombard10 <vincent.lombard@re-cycles-france.fr>
2025-11-19 11:05:30 +0100

    commit

--- resources/views/pdf/product-return.blade.php
+++ resources/views/pdf/product-return.blade.php
@@ -191,8 +191,8 @@
                 </td>
                 <td class="w-third">
                     <h3>Informations de vente</h3>
-                    <div>Date de vente distributeur : {{ $productReturn->bike_sold_at ?? '-' }}</div>
-                    <div>Date de vente consommateur : {{ $productReturn->purchased_at ?? '-' }}</div>
+                    <div>Date de vente distributeur : {{ date('d/m/Y', strtotime($productReturn->bike_sold_at)) ?? '-' }}</div>
+                    <div>Date de vente consommateur : {{ date('d/m/Y', strtotime($productReturn->bike_purchased_at )) ?? '-' }}</div>
                     <div>Commande : {{ $productReturn->order ?? '-' }}</div>
                     <div>Facture : {{ $productReturn->invoice ?? '-' }}</div>
                     <div>Bon de livraison : {{ $productReturn->delivery ?? '-' }}</div>
