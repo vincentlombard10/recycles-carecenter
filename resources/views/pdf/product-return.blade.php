9e855e64df4e63ed8f3e092875ae01a070f1ba44
vincentlombard10 <vincent.lombard@re-cycles-france.fr>
2025-11-19 11:00:33 +0100

    commit

--- resources/views/pdf/product-return.blade.php
+++ resources/views/pdf/product-return.blade.php
@@ -196,7 +196,7 @@
                     <div>Commande : {{ $productReturn->order }}</div>
                     <div>Facture : {{ $productReturn->invoice }}</div>
                     <div>Bon de livraison : {{ $productReturn->delivery }}</div>
-                    <div>Code client : {{ $productReturn->contact?->code }}</div>
+                    <div>Code client : {{ $productReturn->ticket?->contact?->code }}</div>
                 </td>
             </tr>
         </table>
