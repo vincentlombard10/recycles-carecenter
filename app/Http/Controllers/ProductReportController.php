96d63af5aa63b4005f77cedcc3f820b4fe0252c1
vincentlombard10 <vincent.lombard@re-cycles-france.fr>
2025-11-19 13:56:36 +0100

    commit

--- app/Http/Controllers/ProductReportController.php
+++ app/Http/Controllers/ProductReportController.php
@@ -28,10 +28,9 @@ class ProductReportController extends Controller
         foreach($closed_reports as $report){
             $report->duration_time_in_seconds = $report->started_at->diffInSeconds($report->closed_at);
             $report->duration_time_in_minutes = $report->started_at->diffInMinutes($report->closed_at);
-            $report->duration_time_in_minutes_within_business_hours = $report->started_at->diffInHoursFiltered(function(\Carbon\Carbon $date) {
+            $report->duration_time_in_minutes_within_business_hours = $report->started_at->diffInFiltered(function(\Carbon\Carbon $date) {
                 return $date->isWeekday();
             });
-            dd($report->duration_time_in_minutes_within_business_hours);
             $report->save();
         }
 
