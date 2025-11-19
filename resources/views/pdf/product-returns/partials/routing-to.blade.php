dc642c21b20c5f1477b457a4ec32d86f9816bd19
vincentlombard10 <vincent.lombard@re-cycles-france.fr>
2025-11-19 10:54:03 +0100

    commit

--- resources/views/pdf/product-returns/partials/routing-to.blade.php
+++ resources/views/pdf/product-returns/partials/routing-to.blade.php
@@ -1,4 +1,4 @@
-<div>{{ strtoupper($productReturn->routing_to_name ?? $productReturn->recipient->name) }}</div>
+<div>{{ strtoupper($productReturn->routing_to_name ?? $productReturn->to->name) }}</div>
 <div>{{ strtoupper($productReturn->routing_to_address1) }}</div>
 <div>{{ strtoupper($productReturn->routing_to_address2) }}</div>
 <div>{{ $productReturn->routing_to_postcode }} {{ strtoupper($productReturn->routing_to_city) }}</div>
