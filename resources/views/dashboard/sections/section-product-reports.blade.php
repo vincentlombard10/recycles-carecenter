4ba2f9f98bc846c2f7b3ade0ff951f16091a6f97
vincentlombard10 <vincent.lombard@re-cycles-france.fr>
2025-11-20 09:22:10 +0100

    commit

--- resources/views/dashboard/sections/section-product-reports.blade.php
+++ resources/views/dashboard/sections/section-product-reports.blade.php
@@ -3,6 +3,7 @@
     <div class="col-3 mb-3">
         <x-dashboard.counter
             title="en cours"
+            class="Pink"
             :count="$product_reports_in_progress_count" />
     </div>
     <div class="col-3 mb-3">
