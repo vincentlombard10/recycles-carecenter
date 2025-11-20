fccea709da27f3e73436a46764da3e7768d04361
vincentlombard10 <vincent.lombard@re-cycles-france.fr>
2025-11-20 09:32:56 +0100

    commit

--- resources/views/dashboard/sections/section-serials.blade.php
+++ resources/views/dashboard/sections/section-serials.blade.php
@@ -10,4 +10,9 @@
             title="sans item associé"
             :count="$serials_without_item_count" />
     </div>
+    <div class="col-3 mb-3">
+        <x-dashboard.counter
+            title="sans facture associé"
+            :count="$serials_without_invoice_count" />
+    </div>
 </section>
