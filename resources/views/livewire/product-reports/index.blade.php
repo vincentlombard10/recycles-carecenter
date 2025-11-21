af09fdeeb7024bf8da6dd4858878d099ecbf6f41
vincentlombard10 <vincent.lombard@re-cycles-france.fr>
2025-11-21 15:08:28 +0100

    commit

--- resources/views/livewire/product-reports/index.blade.php
+++ resources/views/livewire/product-reports/index.blade.php
@@ -1,4 +1,24 @@
 <div>
+    <div class="row mb-3">
+        <div class="col-lg-3">
+            <select name="status" id="status" class="form-control" wire:model.live="status">
+                <option value="">Tous</option>
+                <option value="init">Retour en attente</option>
+                <option value="pending">En attente</option>
+                <option value="in_progress">En cours</option>
+                <option value="paused">Devis proposé</option>
+                <option value="closed">Terminé</option>
+            </select>
+        </div>
+        <div class="col-lg-3">
+            <select name="environment" id="environment" class="form-control" wire:model.live="environment">
+                <option value="">Tous</option>
+                <option value="production">Réels</option>
+                <option value="sandbox">Fictif</option>
+            </select>
+        </div>
+        <x-pagination :items="$reports" class="col-lg-6"/>
+    </div>
     @if(count($reports))
         <x-pagination :items="$reports" class="mb-3"/>
         <div class="mb-3">
