1138d0d7f8329bfcede8ee280aa8f2492366720a
vincentlombard10 <vincent.lombard@re-cycles-france.fr>
2025-11-27 08:34:45 +0100

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,7 @@ export default {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ['"IBM Plex Sans"', 'Inter', 'Figtree', ...defaultTheme.fontFamily.sans],
             },
         },
     },
