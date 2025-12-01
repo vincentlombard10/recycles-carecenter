8ca7ff9b7d4ecfaa14c06e497ae88c5645d4dc82
vincentlombard10 <vincent.lombard@re-cycles-france.fr>
2025-12-01 09:51:18 +0100

    commit

--- app/Jobs/BaseExportJob.php
+++ app/Jobs/BaseExportJob.php
@@ -43,9 +43,9 @@ class BaseExportJob implements ShouldQueue
             ->setCellAlignment('center')
             ->setCellAlignment('center')
             ->setBorder(new Border(
-                new BorderPart(Border::BOTTOM, Color::BLACK, Border::WIDTH_THIN, Border::STYLE_SOLID),
-                new BorderPart(Border::LEFT, Color::BLACK, Border::WIDTH_THIN, Border::STYLE_SOLID),
-                new BorderPart(Border::RIGHT, Color::BLACK, Border::WIDTH_THIN, Border::STYLE_SOLID),
+                new BorderPart(Border::BOTTOM, Color::RED, Border::WIDTH_THIN, Border::STYLE_SOLID),
+                new BorderPart(Border::LEFT, Color::RED, Border::WIDTH_THIN, Border::STYLE_SOLID),
+                new BorderPart(Border::RIGHT, Color::RED, Border::WIDTH_THIN, Border::STYLE_SOLID),
                 new BorderPart(Border::TOP, Color::BLACK, Border::WIDTH_THIN, Border::STYLE_SOLID)
             ));
     }
