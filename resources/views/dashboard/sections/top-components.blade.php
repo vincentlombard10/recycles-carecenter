451e977582b49f34f86e6b259206bdeece1e0283
vincentlombard10 <vincent.lombard@re-cycles-france.fr>
2025-12-02 14:14:33 +0100

    commit

--- resources/views/dashboard/sections/top-components.blade.php
+++ resources/views/dashboard/sections/top-components.blade.php
@@ -2,64 +2,61 @@
     <h2 class="font-bold text-2xl mb-3">Top retours</h2>
     <div class="grid grid-cols-3 gap-x-6">
         <div class="mb-4">
-            <h2 class="font-bold">Top batteries</h2>
-            <div class="bg-white p-4 rounded-lg">
-                <div class="card-body">
-                    <ul class="list-group list-group-flush">
-                        @forelse($top_batteries as $item)
-                            <li class="list-group-item d-flex justify-content-between align-items-center px-0 py-1">
-                                <div>
-                                    <div><span class="fw-bold text-violet-900">{{ $item->item_itno }}</span></div>
-                                    <div><span class="text-xs">{{ $item->item_itds }}</span></div>
-                                </div>
-                                <span class="text-primary">{{ $item->total }}</span>
-                            </li>
-                        @empty
-                            @lang('Data not available...')
-                        @endforelse
-                    </ul>
-                </div>
-            </div>
+            <h2 class="font-bold mb-3">Top batteries</h2>
+            @if($top_batteries->count())
+                <ul class="bg-white rounded-lg px-4 py-3">
+                    @foreach($top_batteries as $item)
+                        <li class="flex justify-between">
+                            <div>
+                                <div><span class="fw-bold text-violet-900">{{ $item->item_itno }}</span></div>
+                                <div><span class="text-xs">{{ $item->item_itds }}</span></div>
+                            </div>
+                            <span class="font-bold text-2xl text-violet-900">{{ $item->total }}</span>
+                        </li>
+                    @endforeach
+                </ul>
+            @else
+                <span
+                    class="px-3 py-2 bg-violet-50 inline-block w-full rounded-lg text-violet-800 font-semibold">@lang('Data not available...')</span>
+            @endif
         </div>
         <div class="mb-4">
-            <h2 class="font-bold">Top Vélos</h2>
-            <div class="bg-white p-4 rounded-lg">
-                <div class="card-body">
-                    <ul class="list-group list-group-flush">
-                        @forelse($top_components as $item)
-                            <li class="list-group-item d-flex justify-content-between align-items-center px-0 py-1">
-                                <div>
-                                    <div><span class="fw-bold text-violet-900">{{ $item->item_itno }}</span></div>
-                                    <div><span class="text-xs">{{ $item->item_itds }}</span></div>
-                                </div>
-                                <span class="text-primary">{{ $item->total }}</span>
-                            </li>
-                        @empty
-                            @lang('Data not available...')
-                        @endforelse
-                    </ul>
-                </div>
-            </div>
+            <h2 class="font-bold mb-3">Top Vélos</h2>
+            @if($top_components->count())
+                <ul class="bg-white rounded-lg px-4 py-3">
+                    @foreach($top_components as $item)
+                        <li class="flex justify-between">
+                            <div>
+                                <div><span class="font-bold text-violet-900">{{ $item->item_itno }}</span></div>
+                                <div><span class="text-xs">{{ $item->item_itds }}</span></div>
+                            </div>
+                            <span class="font-bold text-2xl text-violet-900">{{ $item->total }}</span>
+                        </li>
+                    @endforeach
+                </ul>
+            @else
+                <span
+                    class="px-3 py-2 bg-violet-50 inline-block w-full rounded-lg text-violet-800 font-semibold">@lang('Data not available...')</span>
+            @endif
         </div>
         <div class="mb-4">
-            <h2 class="font-bold">Top Vélos</h2>
-            <div class="bg-white p-4 rounded-lg">
-                <div class="card-body">
-                    <ul class="list-group list-group-flush">
-                        @forelse($top_bikes as $item)
-                            <li class="list-group-item d-flex justify-content-between align-items-center px-0 py-1">
-                                <div>
-                                    <div><span class="fw-bold text-violet-900">{{ $item->serial_itno }}</span></div>
-                                    <div><span class="text-xs">{{ $item->serial_itds }}</span></div>
-                                </div>
-                                <span class="text-primary">{{ $item->total }}</span>
-                            </li>
-                        @empty
-                            @lang('Data not available...')
-                        @endforelse
-                    </ul>
-                </div>
-            </div>
+            <h2 class="font-bold mb-3">Top Vélos</h2>
+            @if($top_bikes->count())
+                <ul class="bg-white rounded-lg px-4 py-3">
+                    @foreach($top_bikes as $item)
+                        <li class="flex justify-between">
+                            <div>
+                                <div><span class="fw-bold text-violet-900">{{ $item->serial_itno }}</span></div>
+                                <div><span class="text-xs">{{ $item->serial_itds }}</span></div>
+                            </div>
+                            <span class="font-bold text-2xl text-violet-900">{{ $item->total }}</span>
+                        </li>
+                    @endforeach
+                </ul>
+            @else
+                <span
+                    class="px-3 py-2 bg-violet-50 inline-block w-full rounded-lg text-violet-800 font-semibold">@lang('Data not available...')</span>
+            @endif
         </div>
     </div>
 </section>
