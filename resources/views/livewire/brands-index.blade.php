871620d5ada4d7175cd57c2014f44c63b04cf9f7
vincentlombard10 <vincent.lombard@re-cycles-france.fr>
2025-12-03 15:59:33 +0100

    commit

--- resources/views/livewire/brands-index.blade.php
+++ resources/views/livewire/brands-index.blade.php
@@ -1,7 +1,7 @@
 <div>
     <x-pagination :items="$brands" class="mb-3"/>
-    <table class="table">
-        <thead>
+    <table class="table-auto bg-white w-full mb-3">
+        <thead class="bg-violet-900 text-white py-2">
         <tr>
             <th style="width: 8rem;">Code</th>
             <th>Name</th>
@@ -9,9 +9,9 @@
         </thead>
         <tbody>
         @foreach($brands as $brand)
-            <tr>
-                <td>{{ $brand->code  }}</td>
-                <td>{{ $brand->name  }}</td>
+            <tr class="border-b-slate-300 border-b-1 hover:bg-orange-100">
+                <td class="px-3 py-2">{{ $brand->code  }}</td>
+                <td class="px-3 py-2">{{ $brand->name  }}</td>
             </tr>
         @endforeach
         </tbody>
--- resources/views/livewire/groups-index.blade.php
+++ resources/views/livewire/groups-index.blade.php
@@ -1,7 +1,7 @@
 <div>
     <x-pagination :items="$groups" class="mb-3"/>
-    <table class="table">
-        <thead>
+    <table class="table-auto bg-white w-full mb-3">
+        <thead class="bg-violet-900 text-white py-2">
         <tr>
             <th style="width: 8rem;">Code</th>
             <th>Name</th>
@@ -9,9 +9,9 @@
         </thead>
         <tbody>
         @foreach($groups as $group)
-            <tr>
-                <td>{{ $group->code  }}</td>
-                <td>{{ $group->name  }}</td>
+            <tr class="border-b-slate-300 border-b-1 hover:bg-orange-100">
+                <td class="px-3 py-2">{{ $group->code  }}</td>
+                <td class="px-3 py-2">{{ $group->name  }}</td>
             </tr>
         @endforeach
         </tbody>
