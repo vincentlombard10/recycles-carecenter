e65e163383aff3d636283bd151d405ff61dbf9a5
vincentlombard10 <vincent.lombard@re-cycles-france.fr>
2025-12-05 15:03:03 +0100

    commit

--- app/Console/Commands/SyncZendeskCustomFields.php
+++ app/Console/Commands/SyncZendeskCustomFields.php
@@ -2,9 +2,10 @@
 
 namespace App\Console\Commands;
 
+use App\Models\Zendesk\TicketField;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Log;
-
+use Zendesk\API\HttpClient as ZendeskAPI;
 class SyncZendeskCustomFields extends Command
 {
     /**
@@ -27,5 +28,41 @@ class SyncZendeskCustomFields extends Command
     public function handle()
     {
         Log::info("Sync Zendesk custom fields");
+        $client = new ZendeskAPI(config('zendesk.subdomain'));
+        $client->setAuth(config('zendesk.auth_strategy'), [
+            'username' => config('zendesk.username'),
+            'token' => config('zendesk.token')
+        ]);
+
+        $ticketfields = $client->ticketFields()->findAll();
+        foreach($ticketfields->ticket_fields as $ticketfield) {
+            TicketField::updateOrCreate([
+                'id' => $ticketfield->id,
+            ], [
+                'url' => $ticketfield->url,
+                'type' => $ticketfield->type,
+                'title' => $ticketfield->title,
+                'raw_title' => $ticketfield->raw_title,
+                'description' => $ticketfield->description,
+                'raw_description' => $ticketfield->raw_description,
+                'position' => $ticketfield->position,
+                'active' => $ticketfield->active,
+                'required' => $ticketfield->required,
+                'collapsed_for_agents' => $ticketfield->collapsed_for_agents,
+                'regexp_for_validation' => $ticketfield->regexp_for_validation,
+                'title_in_portal' => $ticketfield->title_in_portal,
+                'raw_title_in_portal' => $ticketfield->raw_title_in_portal,
+                'visible_in_portal' => $ticketfield->visible_in_portal,
+                'editable_in_portal' => $ticketfield->editable_in_portal,
+                'required_in_portal' => $ticketfield->required_in_portal,
+                'agent_can_edit' => $ticketfield->agent_can_edit,
+                'tag' => $ticketfield->tag,
+                'created_at' => $ticketfield->created_at,
+                'updated_at' => $ticketfield->updated_at,
+                'removable' => $ticketfield->removable,
+                'key' => $ticketfield->key,
+                'agent_description' => $ticketfield->agent_description,
+            ]);
+        }
     }
 }
--- routes/console.php
+++ routes/console.php
@@ -7,7 +7,7 @@ Schedule::command('items:import ' . \Carbon\Carbon::now()->format('ymd'))
     ->dailyAt('20:00');
 
 Schedule::command('serials:import ' . \Carbon\Carbon::now()->format('ymd'))
-    ->everyMinute();
+    ->hourly();
 
 Schedule::command('contacts:import ' . \Carbon\Carbon::now()->format('ymd'))
     ->dailyAt('20:10');
