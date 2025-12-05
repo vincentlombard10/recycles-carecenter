32969876db01d7bcdfce9a102c6743881692a489
vincentlombard10 <vincent.lombard@re-cycles-france.fr>
2025-12-05 10:23:40 +0100

    commit

--- resources/views/livewire/tickets-index.blade.php
+++ resources/views/livewire/tickets-index.blade.php
@@ -2,47 +2,31 @@
     <x-pagination :items="$tickets" class="mb-3"/>
     <div class="mb-3">
         @foreach($tickets as $ticket)
-            <div class="rcf-card Card_Ticket Card_Ticket__{{ $ticket->status }}">
-                <div class="Card_Ticket_Content">
-                    <div>
-                        <h2 class="fw-semibold mb-1">{{ $ticket->id }}</h2>
-                        @if ($ticket->contact)
-                            <div class="mb-1"><span class="fw-semibold">{{ $ticket->contact->code }}</span> - {{ $ticket->contact->name }}</div>
-                        @endif
-                        <div>{{ stripslashes($ticket->via["channel"]) }}</div>
-                        @if($ticket->comments_count)
-                            <span class="badge" style="background-color: oklch(94.3% 0.029 294.588);">{{ $ticket->comments_count }}<i class="bi bi-chat ms-2"></i>
-</span>
-                        @else
-                            <a href="{{ route('zendesk.tickets.sync', ['id' => $ticket->id]) }}">Get comments</a>
-                        @endif
-                        @if ($ticket->fields_count)
-                            <span class="badge bage-success" style="background-color: oklch(90.1% 0.076 70.697)">TF</span>
-                        @endif
-                        <div><span class="text-primary">{{ date('d/m/Y H:i', $ticket->created_at) }}</span></div>
-                    </div>
-                    <div>
-                        <div><small>1ère réponse : {{ $ticket->first_reply_time_in_minutes }} / {{ $ticket->first_reply_time_in_minutes_within_business_hours }}</small></div>
-                        <div><small>1ère résolution : {{ $ticket->first_resolution_time_in_minutes }} / {{ $ticket->first_resolution_time_in_minutes_within_business_hours }}</small></div>
-                        <div><small>Résolution complète : {{ $ticket->full_resolution_time_in_minutes }} / {{ $ticket->full_resolution_time_in_minutes_within_business_hours }}</small></div>
-                        <div><small>Réponses : {{ $ticket->replies }}</small></div>
-                        <div><small>Réouvertures : {{ $ticket->reopens }}</small></div>
-                    </div>
-                </div>
-                <div>
-                    <div class="d-grid gap-2">
-                        <span class="rcf-badge rcf-badge--{{ strtolower($ticket->status) }}">{{ $ticket->status_label }}</span>
-                        <div class="btn-group">
-                            <button type="button" class="btn btn-light dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-                                Actions
-                            </button>
-                            <ul class="dropdown-menu dropdown-menu-end">
-                                <li>
-                                    <a class="dropdown-item text-end" href="{{ route('support.tickets.show', ['zendeskID' => $ticket->id]) }}">Consulter<i class="bi bi-eye ms-2"></i>
-                                    </a>
-                                </li>
-                            </ul>
+            <div class="bg bg__{{ $ticket->status }} p-3 lg:p-4 mb-2 rounded-md">
+                <div class="grid grid-cols-[auto_8rem] gap-4">
+                    <div class="grid md:grid-cols-[8rem_auto] gap-4">
+                        <div class="column-left">
+                            <h2 class="font-bold text-2xl mb-2">{{ $ticket->id }}</h2>
+                            <span
+                                class="badge-small">{{ $ticket->contact?->code ?? '?' }}</span>
+                            <span
+                                class="badge-small">{{ stripslashes($ticket->via["channel"]) }}</span>
                         </div>
+                        <div class="column-right grid grid-cols-2 gap-4">
+                            <div>Commentaire</div>
+                            <div>
+                                <span class="text-primary">{{ date('d/m/Y H:i', $ticket->created_at) }}</span>
+                                <div><small>1ère réponse : {{ $ticket->first_reply_time_in_minutes }} / {{ $ticket->first_reply_time_in_minutes_within_business_hours }}</small></div>
+                                <div><small>1ère résolution : {{ $ticket->first_resolution_time_in_minutes }} / {{ $ticket->first_resolution_time_in_minutes_within_business_hours }}</small></div>
+                                <div><small>Résolution complète : {{ $ticket->full_resolution_time_in_minutes }} / {{ $ticket->full_resolution_time_in_minutes_within_business_hours }}</small></div>
+                                <div><small>Réponses : {{ $ticket->replies }}</small></div>
+                                <div><small>Réouvertures : {{ $ticket->reopens }}</small></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div
+                            class="badge {{ $ticket->status }} w-full mb-2">{{ $ticket->status_label }}</div>
                     </div>
                 </div>
             </div>
