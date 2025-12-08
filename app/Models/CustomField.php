3592ed65b806052f808c278d158fb193654a449a
vincentlombard10 <vincent.lombard@re-cycles-france.fr>
2025-12-08 13:52:58 +0100

    commit

--- app/Models/CustomField.php
+++ app/Models/CustomField.php
@@ -6,10 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use App\Models\Traits\Scope\TicketFieldScope;
+
 class CustomField extends Model
 {
     use SoftDeletes,
         TicketFieldScope;
+
+    protected $table = 'ticketfields';
 
     protected $fillable = [
         'name',
