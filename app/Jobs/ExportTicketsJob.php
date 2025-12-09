caf0fa8fb3747227ef83f4d2acc675eb475863c9
vincentlombard10 <vincent.lombard@re-cycles-france.fr>
2025-12-09 11:49:44 +0100

    commit

--- app/Jobs/ExportTicketsJob.php
+++ app/Jobs/ExportTicketsJob.php
@@ -320,7 +320,6 @@ use Illuminate\Foundation\Queue\Queueable;
 
     private function getTicketField($ticket, $identifier): string|null
     {
-        return $identifier;
         if ($ticket->fields()->wherePivot('ticketfield_id', $identifier)->exists()) {
             return $ticket->fields()->wherePivot('ticketfield_id', $identifier)->first()->pivot->value;
         }
