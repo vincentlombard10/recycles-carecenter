1b3ecd9377087d498e69b406d4b9fc1061c12fc3
vincentlombard10 <vincent.lombard@re-cycles-france.fr>
2025-12-10 00:45:13 +0100

    commit

--- resources/views/dashboard/sections/section-product-returns.blade.php
+++ resources/views/dashboard/sections/section-product-returns.blade.php
@@ -1,6 +1,6 @@
 <section class="mb-4">
     <h2 class="font-bold text-2xl mb-3">Retours produit</h2>
-    <div class="grid grid-cols-5 gap-x-6">
+    <div class="grid grid-cols-2 lg:grid-cols-4 gap-6">
         <x-dashboard.counter
             title="tous"
             class="Violet"
