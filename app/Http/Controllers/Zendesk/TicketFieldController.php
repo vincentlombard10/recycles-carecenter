ad2221f9625f85485a1f3bf794bb8873568c1f81
vincentlombard10 <vincent.lombard@re-cycles-france.fr>
2025-12-09 09:30:04 +0100

    commit

--- app/Http/Controllers/Zendesk/TicketFieldController.php
+++ app/Http/Controllers/Zendesk/TicketFieldController.php
@@ -10,22 +10,8 @@ class TicketFieldController extends Controller
 {
     public function index()
     {
-        $client = new ZendeskAPI(config('zendesk.subdomain'));
-        $client->setAuth(config('zendesk.auth_strategy'), [
-            'username' => config('zendesk.username'),
-            'token' => config('zendesk.token')
-        ]);
-
         $ticketFields = TicketField::all();
 
-        foreach ($ticketFields as $ticketField) {
-            try {
-                $response = $client->ticketFields()->find($ticketField->id);
-            } catch (\Exception $e) {
-                $ticketField->delete();
-                dump($e->getMessage());
-            }
-        }
         return view('zendesk.fields.index')
             ->with('ticket_fields', $ticketFields);
     }
