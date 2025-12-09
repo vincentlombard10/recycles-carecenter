1398a6bcb1a1b014144be8e683f8905701a0a19c
vincentlombard10 <vincent.lombard@re-cycles-france.fr>
2025-12-10 00:24:41 +0100

    commit

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -1,27 +1,21 @@
 <x-app-layout>
     <x-page-header>
-        <div class="page-header-content">
-            <div class="d-flex align-items-center gap-3">
-                <h1 class="m-0">Mon profil</h1>
-            </div>
-            <div class="d-flex align-items-center gap-3">
-                :)
-            </div>
+        <div class="flex justify-between items-center w-full">
+            <h1 class="m-0">Mon profil</h1>
         </div>
     </x-page-header>
     <x-page-wrapper>
-        <div class="container-fluid p-3">
-            <div class="row mb-3">
-                <div class="col-12 mb-3">
-                    <ul>
-                        <li><span class="fw-semibold">Nom et prénom :</span> {{ auth()->user()->name }}</li>
-                        <li><span class="fw-semibold">Adresse de connexion :</span> {{ auth()->user()->email }}</li>
-                    </ul>
-                </div>
-                <div class="col-12 mb-3">
-                    <a href="{{ route('profile.password.change') }}">Modifier mon mot de passe de connexion</a>
-                </div>
+        <div class="bg-white px-6 py-4 rounded">
+            <div class="mb-3">
+                <ul>
+                    <li><span class="fw-semibold">Nom et prénom :</span> {{ auth()->user()->name }}</li>
+                    <li><span class="fw-semibold">Adresse de connexion :</span> {{ auth()->user()->email }}</li>
+                </ul>
             </div>
+            <div>
+                <a href="{{ route('profile.password.change') }}">Modifier mon mot de passe de connexion</a>
+            </div>
+        </div>
         </div>
     </x-page-wrapper>
 </x-app-layout>
