04322a073bde0ac9477d4b3f4ec660281cd16e7b
vincentlombard10 <vincent.lombard@re-cycles-france.fr>
2025-12-11 15:18:48 +0100

    commit

--- resources/views/layouts/page-header.blade.php
+++ resources/views/layouts/page-header.blade.php
@@ -3,7 +3,7 @@
         {{ $slot }}
         <div class="border-l-1 border-l-slate-200">
             {{ html()->form('POST', route('logout'))->open() }}
-            <button class="w-10 h-10 flex justify-center items-center bg-slate-500 rounded-full text-white hover:bg-rose-600">
+            <button class="w-10 h-10 flex justify-center items-center bg-slate-200 rounded-full text-white hover:bg-rose-600">
                 <i class="bi bi-power"></i><span
                     class="sidebar-link__label"></span></button>
             {{ html()->form()->close() }}
